2b6d9d3910b7f4ce9194a0e384992f86f995a603
baironcayetano <baironjuega01@gmail.com>
2026-06-18 18:11:55 -0600

    solved second  problem

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -34,7 +34,18 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if(value == Data) return true;
+        else if(value < Data)
+        {
+            if(Left is null) return false;
+            else return Left.Contains(value);
+        }
+        else
+        {
+            if(Right is null) return false;
+            else return Right.Contains(value);
+        }
+
     }
 
     public int GetHeight()
